2cffd8e389c0889319458918428dbb583cd7dd2f
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2026-03-25 19:31:31 +0200

    add url

--- frontend/app/api/api.ts
+++ frontend/app/api/api.ts
@@ -2,8 +2,6 @@ import axios, { AxiosError } from "axios";
 
 export type ApiError = AxiosError<{ error: string }>;
 
-const BACKEND_URL = process.env.API_BASE_URL;
-
 export const globalApi = axios.create({
-  baseURL: BACKEND_URL,
+  baseURL: "https://elif-tech-test.onrender.com",
 });
