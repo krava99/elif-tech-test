f645f88ba633ed3b36d4d09af2ae8c5ffbe3ea67
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2026-03-25 19:34:19 +0200

    fix base url

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,5 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
-// export default function Home() {
-//   redirect("/shop");
-// }
-
-export default function page({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <>
-      {children}
-      <div>Домашня сторінка</div>
-    </>
-  );
+export default function Home() {
+  redirect("/shop");
 }
