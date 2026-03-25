46be6c107874bb10f70977e494e6b9c35484744c
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2026-03-25 19:06:06 +0200

    route home

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,16 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/shop");
+// export default function Home() {
+//   redirect("/shop");
+// }
+
+export default function page({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      {children}
+      <div>Домашня сторінка</div>
+    </>
+  );
 }
--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <nav>
         <ul className="flex gap-8 sm:gap-12 md:gap-20 justify-center py-4 cursor-pointer">
           <li>
-            <Link href="/">Shop</Link>
+            <Link href="/shops">Shop</Link>
           </li>
           <li>
             <Link href="/orders">Shopping Cart</Link>
