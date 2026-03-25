dcb3255cbb3d0d1889ef8d955a459b35b0958b66
Dima Kravchenko <dima.kravchenko.1999@gmail.com>
2026-03-25 17:47:05 +0200

    change url

--- frontend/app/api/api.ts
+++ frontend/app/api/api.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from "axios";
 
 export type ApiError = AxiosError<{ error: string }>;
 
+const BACKEND_URL = process.env.API_BASE_URL;
+
 export const globalApi = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
+  baseURL: BACKEND_URL,
 });
